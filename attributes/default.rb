fe1ae193fe1aba61500aaa87c3b0e9196788384a
Hiroyuki Urasoko <h-urasoko@creationline.com>
2015-03-09 18:40:54 +0900

    Update default.rb
    Bump server version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['chef-server']['url'] = \
   'https://web-dl.packagecloud.io/chef/stable/packages/el/6/'\
-'chef-server-core-12.0.3-1.x86_64.rpm'
+'chef-server-core-12.0.5-1.el6.x86_64.rpm'
 default['chef-server']['addons']['packages'] = \
   %w{opscode-manage opscode-reporting }
